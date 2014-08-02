9829a1deb6a4553e56a5be8a6c91aea5a626c355
Ugo Raffaele Piemontese <me@ugopiemontese.eu>
2014-08-02 18:18:21 +0200

    Update and rename part1.sh to install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+(
+apt-get -y install git curl htop
+git clone -b config http://tiledrawer.com/.git/ /usr/local/tiledrawer
+sudo apt-get install -y python-software-properties
+sudo add-apt-repository ppa:mapnik/v2.2.0
+sudo apt-get update
+/usr/local/tiledrawer/setup.sh
+/usr/local/tiledrawer/populate.py -b 39.500 17.000 41.000 19.000 -s https://raw.githubusercontent.com/UgoRaffaele/HighRoad/tiledrawer/tiledrawer.cfg http://geodati.fmach.it/gfoss_geodata/osm/output_osm_regioni/puglia.pbf
+/usr/local/tiledrawer/draw.sh
+) >> /var/log/tiledrawer.log 2>&1
--- part1.sh
+++
@@ -1,4 +0,0 @@
-(
-apt-get -y install git curl htop
-git clone -b config http://tiledrawer.com/.git/ /usr/local/tiledrawer
-) >> /var/log/tiledrawer.log 2>&1
