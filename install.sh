de2fb0697cd3710410ffc6e88bf3e8e96b6cd997
Ugo Raffaele Piemontese <me@ugopiemontese.eu>
2014-08-02 18:33:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ sudo apt-get install -y python-software-properties
 sudo add-apt-repository ppa:mapnik/v2.2.0
 sudo apt-get update
 /usr/local/tiledrawer/setup.sh
+pip install cssutils
+pip install cascadenik
 /usr/local/tiledrawer/populate.py -b 39.500 17.000 41.000 19.000 -s https://raw.githubusercontent.com/UgoRaffaele/HighRoad/tiledrawer/tiledrawer.cfg http://geodati.fmach.it/gfoss_geodata/osm/output_osm_regioni/puglia.pbf
 /usr/local/tiledrawer/draw.sh
 ) >> /var/log/tiledrawer.log 2>&1
